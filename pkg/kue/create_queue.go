5f4fc9f092c223f76e98c8559ea69c49a485ee6b
overcut <overcut@overcut.ai>
2025-07-02 16:02:04 +0000

    feat(tui): add queue creation page and create queue functionality

--- pkg/kue/create_queue.go
+++ pkg/kue/create_queue.go
@@ -1,2 +1,38 @@
 package kue
 
+import (
+    "context"
+    "log"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+// QueueCreateInput represents the parameters used to create a queue.
+type QueueCreateInput struct {
+    // Name of the queue (required)
+    Name string
+
+    // Attributes is an optional map of additional queue attributes.
+    Attributes map[string]string
+}
+
+// CreateQueue creates a new SQS queue and returns its url.
+func CreateQueue(client *sqs.Client, ctx context.Context, input QueueCreateInput) (string, error) {
+
+    if input.Attributes == nil {
+        input.Attributes = map[string]string{}
+    }
+
+    output, err := client.CreateQueue(ctx, &sqs.CreateQueueInput{
+        QueueName:  aws.String(input.Name),
+        Attributes: input.Attributes,
+    })
+    if err != nil {
+        return "", err
+    }
+
+    log.Printf("[CreateQueue] Created queue %s at url %s", input.Name, aws.ToString(output.QueueUrl))
+
+    return aws.ToString(output.QueueUrl), nil
+}
--- pkg/tui/queue_create.go
+++ pkg/tui/queue_create.go
@@ -1,9 +1,13 @@
 package tui
 
 import (
-	"github.com/charmbracelet/huh"
+    "context"
+    "log"
 
-	tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/huh"
+
+    tea "github.com/charmbracelet/bubbletea"
+    kue "github.com/kontrolplane/kue/pkg/kue"
 )
 
 type queueCreateInput struct {
@@ -22,7 +26,7 @@ type queueCreateInput struct {
 
 type queueCreateState struct {
 	input queueCreateInput
-	form  huh.Form
+	form  *huh.Form
 }
 
 func (m model) QueueCreateSwitchPage(msg tea.Msg) (model, tea.Cmd) {
@@ -30,9 +34,58 @@ func (m model) QueueCreateSwitchPage(msg tea.Msg) (model, tea.Cmd) {
 }
 
 func (m model) QueueCreateView() string {
-	return ""
+    if m.state.queueCreate.form == nil {
+        // initialize form lazily because we need dimensions maybe.
+        m.state.queueCreate.form = initQueueCreateForm(&m.state.queueCreate.input)
+    }
+    return m.state.queueCreate.form.View()
 }
 
-func (m model) QueueCreateUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+type submitCreateQueueMsg struct{}
+
+type cancelCreateQueueMsg struct{}
+
+func (m model) QueueCreateUpdate(msg tea.Msg) (model, tea.Cmd) {
+
+    if m.state.queueCreate.form == nil {
+        m.state.queueCreate.form = initQueueCreateForm(&m.state.queueCreate.input)
+    }
+
+    // delegate update to form
+    var cmd tea.Cmd
+    m.state.queueCreate.form, cmd = m.state.queueCreate.form.Update(msg)
+
+    switch msg := msg.(type) {
+    case submitCreateQueueMsg:
+        // call API to create queue
+        _, err := kue.CreateQueue(m.client, m.context, kue.QueueCreateInput{
+            Name: m.state.queueCreate.input.name,
+        })
+        if err != nil {
+            m.error = err.Error()
+        } else {
+            // refresh overview page
+            log.Printf("[QueueCreate] Queue %s created", m.state.queueCreate.input.name)
+        }
+        return m.QueueOverviewSwitchPage(msg)
+    case cancelCreateQueueMsg:
+        return m.QueueOverviewSwitchPage(msg)
+    }
+
+    return m, cmd
+}
+
+func initQueueCreateForm(input *queueCreateInput) huh.Form {
+
+form := huh.NewForm(
+        huh.NewGroup(
+            huh.NewInput().Title("queue name").Value(&input.name).Placeholder("my-queue"),
+        ),
+    )
+
+    form.OnSubmit(func(data map[string]any) {
+        // bubbletea's huh will send a SubmitMsg automatically; intercept using Update above.
+    })
+
+    return form
 }
