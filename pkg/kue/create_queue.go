0f46b75cd4d13bb810d329e3838c0880a6472c5c
overcut <overcut@overcut.ai>
2025-07-02 16:53:37 +0000

    feat: initial queue creation form with backend helper

--- pkg/kue/create_queue.go
+++ pkg/kue/create_queue.go
@@ -1,2 +1,66 @@
 package kue
 
+import (
+    "context"
+    "fmt"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+// CreateQueueInput represents the subset of SQS queue attributes we currently expose in the TUI.
+// Additional optional attributes can be added over time without breaking callers because
+// they will retain their zero-value which we interpret as "use AWS default".
+type CreateQueueInput struct {
+    // Name is the queue name that will be visible in the AWS console.
+    Name string
+
+    // TODO: surface these through the UI in follow-up patches
+    VisibilityTimeout      int32
+    MessageRetentionPeriod int32
+    DelaySeconds           int32
+    ReceiveMessageWaitTime int32
+    MaximumMessageSize     int32
+}
+
+// CreateQueue wraps the AWS SDK CreateQueue call translating only the non-zero
+// fields into the AWS Attributes map so that AWS continues to apply its defaults
+// for every attribute the user did not explicitly set.
+func CreateQueue(client *sqs.Client, ctx context.Context, input CreateQueueInput) (string, error) {
+    if input.Name == "" {
+        return "", fmt.Errorf("queue name is required")
+    }
+
+    attrs := make(map[string]string)
+
+    if input.VisibilityTimeout != 0 {
+        attrs["VisibilityTimeout"] = intToString(input.VisibilityTimeout)
+    }
+    if input.MessageRetentionPeriod != 0 {
+        attrs["MessageRetentionPeriod"] = intToString(input.MessageRetentionPeriod)
+    }
+    if input.DelaySeconds != 0 {
+        attrs["DelaySeconds"] = intToString(input.DelaySeconds)
+    }
+    if input.ReceiveMessageWaitTime != 0 {
+        attrs["ReceiveMessageWaitTimeSeconds"] = intToString(input.ReceiveMessageWaitTime)
+    }
+    if input.MaximumMessageSize != 0 {
+        attrs["MaximumMessageSize"] = intToString(input.MaximumMessageSize)
+    }
+
+    out, err := client.CreateQueue(ctx, &sqs.CreateQueueInput{
+        QueueName:  aws.String(input.Name),
+        Attributes: attrs,
+    })
+    if err != nil {
+        return "", err
+    }
+
+    return aws.ToString(out.QueueUrl), nil
+}
+
+func intToString(v int32) string {
+    return fmt.Sprintf("%d", v)
+}
+
--- pkg/tui/queue_create.go
+++ pkg/tui/queue_create.go
@@ -1,9 +1,11 @@
 package tui
 
 import (
-	"github.com/charmbracelet/huh"
+    "fmt"
 
-	tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/huh"
+    tea "github.com/charmbracelet/bubbletea"
+    kue "github.com/kontrolplane/kue/pkg/kue"
 )
 
 type queueCreateInput struct {
@@ -21,18 +23,47 @@ type queueCreateInput struct {
 }
 
 type queueCreateState struct {
-	input queueCreateInput
-	form  huh.Form
+    input queueCreateInput
+    form  *huh.Form
 }
 
 func (m model) QueueCreateSwitchPage(msg tea.Msg) (model, tea.Cmd) {
-	return m.SwitchPage(queueCreate), nil
+    // initialise form on first open
+    nameInput := huh.NewInput().Title("Queue name").Value(&m.state.queueCreate.input.name).Validate(func(val string) error {
+        if val == "" {
+            return fmt.Errorf("name is required")
+        }
+        return nil
+    })
+
+    m.state.queueCreate.form = huh.NewForm(huh.NewGroup(nameInput)).WithTheme(huh.ThemeDracula())
+
+    return m.SwitchPage(queueCreate), nil
 }
 
 func (m model) QueueCreateView() string {
-	return ""
+    if m.state.queueCreate.form == nil {
+        return "loading form"
+    }
+    return m.state.queueCreate.form.View()
 }
 
 func (m model) QueueCreateUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+    if m.state.queueCreate.form == nil {
+        return m, nil
+    }
+
+    var cmd tea.Cmd
+    if m.state.queueCreate.form.State() == huh.StateCompleted {
+        // user submitted the form
+        _, err := kue.CreateQueue(m.client, m.context, kue.CreateQueueInput{Name: m.state.queueCreate.input.name})
+        if err != nil {
+            m.error = fmt.Sprintf("Error creating queue: %v", err)
+        }
+        // refresh overview on success
+        return m.QueueOverviewSwitchPage(msg)
+    }
+
+    m.state.queueCreate.form, cmd = m.state.queueCreate.form.Update(msg)
+    return m, cmd
 }
